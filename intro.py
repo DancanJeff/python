8170e69672c353454494be58406d09a9032c95f5
DancanJeff <danjeff254@gmail.com>
2025-03-04 15:01:53 +0300

    my first commit

---
+++ intro.py
@@ -0,0 +1,14 @@
+age=25
+height=1.65
+name="Dancan"
+is_student= True
+
+print("name:",name)
+print("height:",height)
+print("age:",age)
+print("is_student:",is_student)
+
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))
